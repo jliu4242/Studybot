d1a29f967d4cb9bf669758447e9d963c535ac083
Jon Liu <jonliu4242@gmail.com>
2025-09-04 16:39:18 -0700

    generate page base

--- src/app/pages/generate/page.js
+++ src/app/pages/generate/page.js
@@ -1,14 +1,30 @@
 'use client'
 import { useState } from 'react';
+import '../../styles/generate.css'
 
 
 export default function uploadPage() {
     const [textbooks, setTextbooks] = useState('');
 
     return (
-        <div className='w-screen'>
+        <div className='min-h-screen w-screen bg-black'>
             <div className='text-center p-10'>
-                <h2 className=''>Generate</h2>
+                <h2 className='text-3xl text-white'>Upload your notes or example questions</h2>
+            </div>
+
+            <div className='mx-auto w-full max-w-xl text-left shadow-lg space-y-8 divide-y
+                            divide-slate-200/5 bg-slate-900 sm:overflow-hidden sm:rounded-md'>
+                    <div>
+                        <label id='label'>desired chapter/section names</label>
+                        <textarea 
+                            placeholder = "Example: Implicit Differentiation"
+                            onChange={(e) => setChapters(e.target.value)}>
+                        </textarea>
+                    </div>
+                    <div>
+                        <label className='block text-white mb-2'>Upload</label>
+                        <button>Generate</button>
+                    </div>
             </div>
         </div>
     )
--- src/app/pages/upload/page.js
+++ src/app/pages/upload/page.js
@@ -5,15 +5,16 @@ import '../../styles/globals.css'
 
 export default function UploadPage() {
     const [file, setFile] = useState(null);
+    const [uploadType, setUploadType] = useState('');
 
     return (
-        <div className='min-h-screen w-screen bg-slate-900'>
+        <div className='min-h-screen w-screen bg-black'>
             <div className='text-center p-10'>
                 <h2 className='text-3xl text-white'>Upload your notes or example questions</h2>
             </div>
 
             <div className='mx-auto w-full max-w-xl text-left shadow-lg space-y-8 divide-y
-                            divide-slate-200/5 bg-slate-800 sm:overflow-hidden sm:rounded-md'>
+                            divide-slate-200/5 bg-slate-900 sm:overflow-hidden sm:rounded-md'>
                 <div className=' px-4 py-5 divide-slate-200/5 sm:p-6'>
                     <div>
                         <label className='block text-white mb-2'>Select file</label>
@@ -27,6 +28,13 @@ export default function UploadPage() {
                                      file:border file:border-slate-600 file:rounded
                                      file:bg-slate-700 file:text-white hover:file:bg-slate-600'
                         />
+                    </div>
+                    <div>
+                        <label>Upload Type:</label>
+                        <select id='Upload Type' name='Upload Type' className='text-black'>
+                            <option value="Notes">Notes</option>
+                            <option value="Example Questions">Example Questions</option>
+                        </select>
                     </div>
                     <div>
                         <label className='block text-white mb-2'>Upload</label>
