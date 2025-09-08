a9adb1e64a4839ee37ac6b6de2bdb2a65a3af20e
Jon Liu <jonliu4242@gmail.com>
2025-09-08 16:17:52 -0700

    Information Retrieval

--- src/app/api/generate.py
+++ src/app/api/generate.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, Body
 from fastapi.middleware.cors import CORSMiddleware
 from openai import OpenAI
 from dotenv import load_dotenv
@@ -61,7 +61,48 @@ async def upload_notes(file: UploadFile = File(...)):
     print('finished')
 
 @app.post("/generate-questions")
-async def generate_questions() {
+async def generate_questions(text: str = Body(...)):
+    print('connected: ' + text)
+
+    response = client.embeddings.create(
+        model='text-embedding-3-large',
+        input=text
+    )
+
+    vector = np.array(response.data[0].embedding, dtype=float)
+    print(vector)
     
-}
+    norm = np.linalg.norm(vector)
+    normalized_vector = vector / norm
+    print(normalized_vector)
+
+    vectors = [doc["embedding"] for doc in collection.find({})]
+
+    scores = []
+    # Calculating cosine similarity
+    for vector in vectors:
+        normalized_vector, vector = np.array(normalized_vector), np.array(vector)
+        res = np.dot(normalized_vector, vector)
+        scores.append(res)
+
+    best_index = np.argmax(scores)
+    context_text = documents[best_index]["text"]
+
+    prompt = f""" You are a helpful tutor that creates questions to help students learn. Based only on the following text, generate 5 questions.
+
+    Text:
+    \"\"\"{context_text}\"\"\"
+
+    Questions:
+    """
+
+    reponse = client.chat.completions.create(
+        model='gpt-3.5-turbo',
+        messages=[
+            {'role': 'system', 'content': 'You are a tutor who creates test questions'},
+            {'role': 'user', 'content': prompt}
+        ]
+    )
+
+    print (response.choices[0].message.contet)
     
